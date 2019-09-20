388662a1c6a56b1a4130554befc58921cad01acf
tr3vbr0w <trevbrow@live.com>
2019-09-20 14:31:52 -0600

    init word constructor function

--- assets/word.js
+++ assets/word.js
@@ -1,1 +1,15 @@
-var letter = require('./letter');
+// Require constructor from letter.js
+var letter = require('./letter.js');
+
+//Initialize Word constructor
+
+var Word = function(newWord) {
+    this.wordArr = newWord.split("");
+    this.letterArr = [];
+    this.guesses = 12;
+
+    //This segment of the constructor sets 
+
+    //This segment of the constructor sets letterArr to break up the selected word into 
+}
+
