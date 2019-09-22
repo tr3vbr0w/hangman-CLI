e4c5d8a993e1ef867786fb50af099879167b6a0e
tbsteeze <trevbrow@live.com>
2019-09-21 21:47:26 -0600

    constructor functions completed

--- assets/word.js
+++ assets/word.js
@@ -1,15 +1,40 @@
 // Require constructor from letter.js
 var letter = require('./letter.js');
 
+var wordArr = ['Timponogos','Mt Olympus', 'Lone Peak', 'Ninety-Nine Ninety', 'Jupiter Peak', 'Clayton Peak', 'Kessler Peak','Grandeur Peak', 'Mt Superior']
 //Initialize Word constructor
 
-var Word = function(newWord) {
+var Word = function(wordArr) {
     this.wordArr = newWord.split("");
     this.letterArr = [];
     this.guesses = 12;
-
+    //This segment of the constructor sets letterArr to break up the selected word into an array of letters, displaying either the guessed letter or the placeholder
+    for (let i = 0; i < wordArr.length; i++) {
+        var newLetter = new Letter(this.wordArr[i]);
+        this.letterArr.push(newLetter);
+    }
     //This segment of the constructor sets 
+    this.returnWord = function(){
+        var word;
+        for (let i = 0; i < letterArr.length; i++) {
+            word += this.letterArr[i].returnLetter();
+        }
+        return word;
+    }
+    // Function checks guess against each letter in letterArr
+    this.guess = function(input){
+        var correct = false;
+        for (let i = 0; i < this.letterArr.length; i++) {
+            if(!this.letterArr[i].solved){
+                this.letterArr[i].guess(input);
+                if(this.letterArr[i].solved){
+                    correct = true;
+                }
+            }
+            
+        }
+        
+    }
 
-    //This segment of the constructor sets letterArr to break up the selected word into 
 }
 
