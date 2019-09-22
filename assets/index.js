8e16e239e331ed68070060b28dc0d8dfc33f11ae
tbsteeze <trevbrow@live.com>
2019-09-22 12:26:54 -0600

    Successfully calling constructor functions into index.js

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,19 @@
 var Word = require('./word.js');
 var inquirer =require('inquirer');
+
+var wordArr = ['Timponogos','Mt Olympus', 'Lone Peak', 'Ninety-Nine Ninety', 'Jupiter Peak', 'Clayton Peak', 'Kessler Peak','Grandeur Peak', 'Mt Superior']
+var currentWord = [];
+var currentWordString = "";
+
+
+function newWord(){
+    var rand = Math.floor(Math.random() * wordArr.length);
+    currentWord = new Word(wordArr[rand]);
+    currentWordString = wordArr[rand];
+
+    console.log(currentWord);
+    console.log(currentWordString);
+    
+
+}
+newWord();
--- assets/word.js
+++ assets/word.js
@@ -1,16 +1,15 @@
 // Require constructor from letter.js
-var letter = require('./letter.js');
+var Letter = require('./letter.js');
 
-var wordArr = ['Timponogos','Mt Olympus', 'Lone Peak', 'Ninety-Nine Ninety', 'Jupiter Peak', 'Clayton Peak', 'Kessler Peak','Grandeur Peak', 'Mt Superior']
 
 //Initialize Word constructor
 
-var Word = function(wordArr) {
+var Word = function(newWord) {
     this.wordArr = newWord.split("");
     this.letterArr = [];
     this.guesses = 12;
     //This segment of the constructor sets letterArr to break up the selected word into an array of letters, displaying either the guessed letter or the placeholder
-    for (let i = 0; i < wordArr.length; i++) {
+    for (let i = 0; i < this.wordArr.length; i++) {
         var newLetter = new Letter(this.wordArr[i]);
         this.letterArr.push(newLetter);
     }
